c276fb01478884b49d36d6480387bf294083b797
Elena <elenela@yandex.ru>
2023-08-21 16:31:19 +0300

    практика

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Harry");
+        names.add("Ron");
+        names.add("Hermione");
+        names.add("Draco");
+        System.out.println(names);
+        System.out.println( removeLetter(names));
+
+    }
+
+    public static List<String> removeLetter (List<String> names){
+        return names.stream()
+                .map(x->x.substring(1))
+                .sorted().collect(Collectors.toList());
+    }
+
+
+
+
+}
